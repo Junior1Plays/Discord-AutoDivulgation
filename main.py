dd82d377790395820447997b049f77f03cc66417
Junior Schueller <50379079+JuniorSchueller@users.noreply.github.com>
2024-05-14 23:23:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,68 @@
+from colorama import Fore, Back, Style, init
+import datetime
+import json
+import os
+import pyperclip
 import requests
 import time
 
-convite = input("Convite: ")
-token = input("Token: ")
-exp = ["1068292377539792987", "741289313614037092", "615658321780801558", "667407426516942859", "616006505245835273"]
-explite = ["680848027870822405", "680848033378074644", "681143447771676775", "681143568559243285", "680848066823454737"]
-divagora = ["954981586049306675", "954981570945642496", "954981557410607124", "834919284609777744"]
-divelite = ["1074109938282020945", "1000060482243268768", "1005228529664925727", "993871733155172443", "993871320632791070", "993872066547818546", "1003706842456789032", "1074121968045006889", "1008381320793169941"]
-div = ["1090380916553171054", "1090381190910972025"]
+token = ''
+invite = """"""
+channels = {}
 
-def sendinvite():
-  body = {
-    "authorization": token
-  }
-  invite = {
-    "content": convite
-  }
-  for channel in exp:
-    r = requests.post("https://discord.com/api/v10/channels/" + channel + "/messages", headers=body, data=invite)
-    print("DIVULGADO NO CANAL " + channel + " DA EXP")
-  for channel in explite:
-    r = requests.post("https://discord.com/api/v10/channels/" + channel + "/messages", headers=body, data=invite)
-    print("DIVULGADO NO CANAL " + channel + " DA EXP LITE")
-  for channel in divagora:
-    r = requests.post("https://discord.com/api/v10/channels/" + channel + "/messages", headers=body, data=invite)
-    print("DIVULGADO NO CANAL " + channel + " DO DIVULGUE AGORA")
-  for channel in divelite:
-    r = requests.post("https://discord.com/api/v10/channels/" + channel + "/messages", headers=body, data=invite)
-    print("DIVULGADO NO CANAL " + channel + " DO DIVULGAÇÃO & ELITE")
-  for channel in div:
-    r = requests.post("https://discord.com/api/v10/channels/" + channel + "/messages", headers=body, data=invite)
-    print("DIVULGADO NO CANAL " + channel + " DO DIVULGUE!")
-  time.sleep(18000)
-  sendinvite()
-sendinvite()
+with open('token.txt', 'r') as arquivo:
+    conteudo = arquivo.read()
+    token = conteudo
+
+with open('invite.txt', 'r') as arquivo:
+    conteudo = arquivo.read()
+    pyperclip.copy(conteudo)
+    invite = pyperclip.paste()
+
+with open('channels.json', 'r') as arquivo:
+    conteudo = arquivo.read()
+    channels = json.loads(conteudo)
+
+init(autoreset=True)
+
+def send_message(channel_id, div_server, message):
+    if channel_id in channels[div_server]:
+        body = {
+            'content': message
+        }
+        headers = {
+            "authorization": token
+        }
+        requests.post(f'https://discord.com/api/v10/channels/{channel_id}/messages', headers=headers, json=body)
+        print(f"Divulgado no ID {channel_id} ({div_server})")
+
+def imprimirChaves(json_data):
+    for chave, subchaves in json_data.items():
+        print(f"Enviando convites em: {chave}")
+        for canal in subchaves:
+            send_message(canal, chave, invite)
+
+def send_messages():
+    imprimirChaves(channels)
+    
+    agora = datetime.datetime.now()
+    horario_daqui_a_5_minutos = agora + datetime.timedelta(minutes=5)
+    horario_formatado = horario_daqui_a_5_minutos.strftime("%H:%M:%S")
+
+    print(f"Próxima divulgação: {horario_formatado}")
+    time.sleep(326)
+    send_messages()
+os.system('title Discord Auto-Div')
+
+print(Fore.RED + 'Discord Auto-Div v1.0\nMade by Junior Schueller\n')
+
+headers = {
+    'authorization': token
+}
+
+userdata_req = requests.get('https://discord.com/api/v10/users/@me', headers=headers)
+userdata_json = userdata_req.json()
+print("Olá, " + Fore.CYAN + userdata_json['username'] + Fore.RESET + "!")
+os.system('pause')
+
+send_messages()
